c726a72fac9a3e832ce788749a4fef1d69b44f95
Patwicku <punzalan2233@gmail.com>
2024-07-13 03:28:03 +0800

    style categories

--- resources/views/livewire/frontend/listing-page-section.blade.php
+++ resources/views/livewire/frontend/listing-page-section.blade.php
@@ -38,12 +38,12 @@ new class extends Component {
                                     <span class="text-gray-600">{{ $sub_category->name }}</span>
                                 </label>
                             @endforeach
-                            
                         </div>
                     </div>
                 @endforeach
             </div>
         </div>
+
 
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
             <!-- Replace with your product cards or items -->
