d4833c97437b14197da2e357cbceb57002aa31d5
Patwicku <punzalan2233@gmail.com>
2024-07-21 17:55:52 +0800

    category size

--- resources/views/livewire/frontend/category-page.blade.php
+++ resources/views/livewire/frontend/category-page.blade.php
@@ -28,7 +28,7 @@ new class extends Component {
             <div class="flex-1 min-w-72 max-w-xl flex flex-col gap-8 xsm:min-w-80">
                 @foreach($chunk as $category)
                     <div class="border border-gray-200 shadow-sm bg-gray-50 rounded-lg p-5">
-                        <p class="mb-4 font-medium text-gray-700">{{ $category->name }}</p>
+                        <p class="mb-4 text-lg font-medium text-gray-700">{{ $category->name }}</p>
                         <ol class="grid grid-cols-1 sm:grid-cols-2 gap-2 list-disc list-inside">
                             @foreach($category->subCategories as $index => $subCategory)
                                 <li class="text-gray-600 break-words">
