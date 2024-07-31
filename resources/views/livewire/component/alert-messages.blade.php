02cddf023092d19c359ea9598c180f7cce88bb8a
Patwicku <punzalan2233@gmail.com>
2024-08-01 06:46:54 +0800

    add console to session

--- resources/views/livewire/component/alert-messages.blade.php
+++ resources/views/livewire/component/alert-messages.blade.php
@@ -8,10 +8,10 @@ new class extends Component {
 
 <div class="space-y-8">
     <!-- Success Alert -->
-    <div class="hidden items-center justify-between bg-green-100 border-l-4 border-green-500 text-green-700 p-4 rounded-lg"
+    <div class="items-center justify-between hidden p-4 text-green-700 bg-green-100 border-l-4 border-green-500 rounded-lg"
         role="alert" id="alert-success">
         <div class="flex items-center">
-            <svg class="w-6 h-6 mr-2 fill-current text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <svg class="w-6 h-6 mr-2 text-green-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M9 21H5V11H2.5L12 3.25L21.5 11H19V21H15V13H9V21Z"/>
             </svg>
             <div>
@@ -20,16 +20,16 @@ new class extends Component {
             </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="svg-x size-6 cursor-pointer hover:opacity-70">
+            class="cursor-pointer svg-x size-6 hover:opacity-70">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
     </div>
 
     <!-- Info Alert -->
-    <div class="hidden items-center justify-between bg-blue-100 border-l-4 border-blue-500 text-blue-700 p-4 rounded-lg"
+    <div class="items-center justify-between hidden p-4 text-blue-700 bg-blue-100 border-l-4 border-blue-500 rounded-lg"
         role="alert" id="info-message">
         <div class="flex items-center">
-            <svg class="w-6 h-6 mr-2 fill-current text-blue-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <svg class="w-6 h-6 mr-2 text-blue-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M12 2C6.48 2 2 6.48 2 12S6.48 22 12 22 22 17.52 22 12 17.52 2 12 2ZM11 17H13V11H11V17ZM11 9H13V7H11V9Z"/>
             </svg>
             <div>
@@ -38,16 +38,16 @@ new class extends Component {
             </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="svg-x size-6 cursor-pointer hover:opacity-70">
+            class="cursor-pointer svg-x size-6 hover:opacity-70">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
     </div>
 
     <!-- Error Alert -->
-    <div class="hidden items-center justify-between bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-lg"
+    <div class="items-center justify-between hidden p-4 text-red-700 bg-red-100 border-l-4 border-red-500 rounded-lg"
         role="alert" id="error-message">
         <div class="flex items-center">
-            <svg class="w-6 h-6 mr-2 fill-current text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <svg class="w-6 h-6 mr-2 text-red-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                 <path d="M11 15H13V17H11V15ZM11 7H13V13H11V7ZM12 2C6.48 2 2 6.48 2 12S6.48 22 12 22 22 17.52 22 12 17.52 2 12 2Z"/>
             </svg>
             <div>
@@ -56,7 +56,7 @@ new class extends Component {
             </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="svg-x size-6 cursor-pointer hover:opacity-70">
+            class="cursor-pointer svg-x size-6 hover:opacity-70">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
         </svg>
     </div>
@@ -65,6 +65,7 @@ new class extends Component {
     <script>
         document.addEventListener('livewire:init', () => {
             Livewire.on('alert-success', (events) => {
+                console.log('alert success session running');
                 document.getElementById('alert-success').classList.remove('hidden');
                 document.getElementById('alert-success').classList.add('flex');
                 if(events.route){
