3b1b8aa97248f7ae2e2df43f60794b6096c8abe5
Patwicku <punzalan2233@gmail.com>
2024-08-01 06:02:26 +0800

    logout

--- resources/views/livewire/frontend/navbar.blade.php
+++ resources/views/livewire/frontend/navbar.blade.php
@@ -19,10 +19,10 @@ new class extends Component {
 
 <div class="sticky top-0 z-20" x-data="{ sidebarOpen: false }">
     <div class="bg-[#246567]">
-        <div class="sm:container mx-auto py-2 px-2 sm:px-0">
-            <div class="flex justify-between items-center">
+        <div class="px-2 py-2 mx-auto sm:container sm:px-0">
+            <div class="flex items-center justify-between">
                 <div class="flex items-center gap-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white size-8 text-dark cursor-pointer hover:opacity-70"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white cursor-pointer size-8 text-dark hover:opacity-70"
                         @click="sidebarOpen = true;">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
@@ -30,7 +30,7 @@ new class extends Component {
                         <img class="max-w-24" src="{{ asset('frontend/alaska.png') }}" alt="">
                     </a>
                 </div>
-                <div class="flex justify-end items-center gap-4 w-full xl:px-0">
+                <div class="flex items-center justify-end w-full gap-4 xl:px-0">
                 @guest
                     <a class="relative" href="{{ route('register') }}" wire:navigate>
                         <div class="font-bold text-white border-2 border-green-300 bg-[#246567] px-4 py-2 rounded-full shadow-md hover:bg-[#1B494D] hover:border-white transition duration-300">
@@ -38,32 +38,32 @@ new class extends Component {
                         </div>
                     </a>
                 @endguest
-                    {{-- <div class="relative w-full max-w-60 p-1 overflow-hidden md:max-w-96 ">
-                        <input class="w-full rounded-full py-2 pr-12 pl-4 focus:ring-2" type="search" wire:model="search" placeholder="Search..."
+                    {{-- <div class="relative w-full p-1 overflow-hidden max-w-60 md:max-w-96 ">
+                        <input class="w-full py-2 pl-4 pr-12 rounded-full focus:ring-2" type="search" wire:model="search" placeholder="Search..."
                             x-on:change="$dispatch('search-on', { val: $event.target.value})">
-                        <button class="absolute inset-y-0 right-2 flex items-center pr-3">
+                        <button class="absolute inset-y-0 flex items-center pr-3 right-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                             </svg>
                         </button>
                     </div> --}}
-                    {{-- <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('welcome') }}" wire:navigate>Home</a>
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('categories') }}" wire:navigate>Categories</a>
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('listing-page') }}" wire:navigate>Listing Page</a>
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('about-us') }}" wire:navigate>About Us</a>
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="/#get-in-touch" wire:navigate>Get In Touch</a>
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('advertise-with-us') }}" wire:navigate>Advertise with us</a>
+                    {{-- <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('welcome') }}" wire:navigate>Home</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('categories') }}" wire:navigate>Categories</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('listing-page') }}" wire:navigate>Listing Page</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('about-us') }}" wire:navigate>About Us</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="/#get-in-touch" wire:navigate>Get In Touch</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('advertise-with-us') }}" wire:navigate>Advertise with us</a>
                     @guest
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 lg:block" href="{{ route('register') }}" wire:navigate>Sell</a>
+                    <a class="hidden font-medium text-slate-100 hover:text-slate-800 lg:block" href="{{ route('register') }}" wire:navigate>Sell</a>
                     @endguest
                     @auth
-                    <a class="hidden text-slate-100 font-medium hover:text-slate-800 cursor-pointer lg:block" href="{{ route('dashboard') }}" wire:navigate>Dashboard</a>
+                    <a class="hidden font-medium cursor-pointer text-slate-100 hover:text-slate-800 lg:block" href="{{ route('dashboard') }}" wire:navigate>Dashboard</a>
                     @endauth --}}
                 </div>
             </div>
         </div>
     </div>
-    <div class="fixed top-0 h-dvh w-80 bg-gray-900 bg-opacity-90 p-6 z-10 overflow-y-auto"
+    <div class="fixed top-0 z-10 p-6 overflow-y-auto bg-gray-900 h-dvh w-80 bg-opacity-90"
         x-cloak
         x-transition:enter="transition ease-out duration-300"
         x-transition:enter-start="-translate-x-full"
@@ -74,50 +74,50 @@ new class extends Component {
         x-show="sidebarOpen"
         @click.outside="sidebarOpen = false">
         <div class="space-y-4 ">
-            <div class="flex justify-between items-center md:justify-end">
+            <div class="flex items-center justify-between md:justify-end">
                 <a class="block md:hidden" href="/" wire:navigate>
                     <img class="max-w-24" src="{{ asset('frontend/alaska.png') }}" alt="">
                 </a>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-slate-300 cursor-pointer hover:text-white"
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cursor-pointer size-8 text-slate-300 hover:text-white"
                 @click="sidebarOpen = false;">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                 </svg>
             </div>
             <div class="flex flex-col gap-5 text-white">
-                <a class="relative group px-1" href="{{ route('welcome') }}" wire:navigate>
+                <a class="relative px-1 group" href="{{ route('welcome') }}" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">Home</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
-                <a class="relative group px-1" href="{{ route('categories') }}" wire:navigate>
+                <a class="relative px-1 group" href="{{ route('categories') }}" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">Categories</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
-                <a class="relative group px-1" href="{{ route('listing-page') }}" wire:navigate>
+                <a class="relative px-1 group" href="{{ route('listing-page') }}" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">Listing Page</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
-                <a class="relative group px-1" href="{{ route('about-us') }}" wire:navigate>
+                <a class="relative px-1 group" href="{{ route('about-us') }}" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">About Us</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
-                <a class="relative group px-1" href="/#get-in-touch" wire:navigate>
+                <a class="relative px-1 group" href="/#get-in-touch" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">Get In Touch</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
-                <a class="relative group px-1" href="{{ route('advertise-with-us') }}" wire:navigate>
+                <a class="relative px-1 group" href="{{ route('advertise-with-us') }}" wire:navigate>
                     <span class="transition-opacity group-hover:opacity-70">Advertise With Us</span>
-                    <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                    <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                 </a>
                 <div class="flex flex-col gap-5 mt-10">
                 @guest
                     @else
-                    <a class="relative group px-1" href="{{ route('dashboard') }}" wire:navigate>
+                    <a class="relative px-1 group" href="{{ route('dashboard') }}" wire:navigate>
                         <span class="transition-opacity group-hover:opacity-70">Dashboard</span>
-                        <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                        <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                     </a>
-                    <a class="relative group px-1" href="{{ route('dashboard') }}" wire:cilck="logout">
+                    <a class="relative px-1 group" wire:cilck="logout">
                         <span class="transition-opacity group-hover:opacity-70">Logout</span>
-                        <span class="absolute -bottom-3 left-0 w-0 transition-all h-1 bg-white group-hover:w-full"></span>
+                        <span class="absolute left-0 w-0 h-1 transition-all bg-white -bottom-3 group-hover:w-full"></span>
                     </a>
                 @endguest
                 </div>
