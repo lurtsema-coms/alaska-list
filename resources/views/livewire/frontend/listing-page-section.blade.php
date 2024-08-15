f6b08a91a4bee1bdb96acdef42e1f92795de95bf
Patwicku <punzalan2233@gmail.com>
2024-08-15 09:02:56 +0800

    grid layout

--- resources/views/livewire/frontend/listing-page-section.blade.php
+++ resources/views/livewire/frontend/listing-page-section.blade.php
@@ -180,7 +180,7 @@ new class extends Component {
                     </div>
                 </div>
 
-                {{-- <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     @foreach ($categories as $category)
                         <div class="p-4 bg-gray-100 rounded-lg shadow-md" wire:key="{{ 'category-listing-'.$category->id }}">
                             <h3 class="mb-2 text-lg font-medium text-gray-700">{{ $category->name }}</h3>
@@ -197,29 +197,7 @@ new class extends Component {
                             </div>
                         </div>
                     @endforeach
-                </div> --}}
-                <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                    @foreach ($categories as $category)
-                        <div class="p-4 bg-gray-100 rounded-lg shadow-md" wire:key="{{ 'category-listing-'.$category->id }}">
-                            <h3 class="mb-2 text-lg font-medium text-gray-700">{{ $category->name }}</h3>
-                            <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                                @foreach($category->subCategories as $sub_category)
-                                    <label class="flex items-center space-x-2 cursor-pointer" wire:key="{{ 'sub-categ-listing-'.$sub_category->id }}">
-                                        <input type="checkbox"
-                                            class="w-5 h-5 text-blue-600"
-                                            wire:model.change="sc_names"
-                                            value="{{ $sub_category->name }}">
-                                        <span class="text-gray-600">{{ $sub_category->name }}</span>
-                                    </label>
-                                @endforeach
-                            </div>
-                        </div>
-                    @endforeach
                 </div>
-
-
-
-
             </div>
         </div>
 
