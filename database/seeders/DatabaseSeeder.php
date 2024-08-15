b160a524905ad0229a989f3d895f358d58a444ae
unknown <gab.quing29@gmail.com>
2024-08-16 01:43:50 +0800

    done with seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(AddUserSeeder::class);
         $this->call(AddCategorySeeder::class);
+        $this->call(AddSubCategorySeeder::class);
     }
 }
