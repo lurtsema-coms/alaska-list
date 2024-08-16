6bd008f31e3cf9ae96064bab90e3825008499178
Patwicku <punzalan2233@gmail.com>
2024-08-16 08:49:41 +0800

    px report a problem

--- resources/views/livewire/frontend/report-listing.blade.php
+++ resources/views/livewire/frontend/report-listing.blade.php
@@ -32,7 +32,7 @@ new class extends Component {
             x-transition:leave-end="opacity-0"
             class="flex fixed inset-0 z-[99] w-screen h-screen overflow-auto bg-white"
             >
-            <button @click="fullscreenModal=false" class="absolute top-0 right-0 z-30 flex items-center justify-center px-3 py-2 mt-3 mr-3 space-x-1 text-xs font-medium uppercase border rounded-md border-neutral-200 text-neutral-600 hover:bg-neutral-100">
+            <button @click="fullscreenModal=false" class="absolute top-0 right-0 z-30 flex items-center justify-center px-3 py-2 mt-3 mr-8 space-x-1 text-xs font-medium uppercase border rounded-md border-neutral-200 text-neutral-600 hover:bg-neutral-100">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
                 <span>Close</span>
             </button>
