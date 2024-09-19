eeb3eac9a8e7f785ac11e72fab367f98acdc486a
unknown <gab.quing29@gmail.com>
2024-09-20 02:31:27 +0800

    sidemessage in progress

--- resources/views/livewire/frontend/sidenav-message.blade.php
+++ resources/views/livewire/frontend/sidenav-message.blade.php
@@ -7,21 +7,23 @@ new class extends Component {
 }; ?>
 
 <div>
-    <div class="flex flex-row items-center px-8 py-10 mt-10 bg-white shadow-sm rounded-xl">
-        <div class="mb-12 text-center">
-            <h1 class="text-lg font-bold text-center  text-slate-700">
-                Share Your Treasures with Us
-            </h1>
-            <p class="max-w-3xl mx-auto mt-4 text-md text-slate-500">
-                Join our community and start listing your valuable items today. It’s fast, easy, and secure.
-            </p>
+    <div class="flex flex-row items-center px-6 py-8 mt-10  shadow-sm rounded-xl">
+        <div class="">
+            <div class=" max-w-6xl px-8">
+                <h1 class="text-xl font-bold text-[#2171a7] underline">
+                    Share Your Treasures with Us
+                </h1>
+                <p class="max-w-3xl mx-auto mt-4 text-md text-slate-500">
+                    Join our community and start listing your valuable items today. It’s fast, easy, and secure.
+                </p>
+            </div>
             <div class="flex flex-col items-center max-w-6xl gap-8 px-8 py-10 mx-auto ">
                 <div class="flex flex-col transition-transform transform hover:scale-105">
                     <div class="flex flex-row items-center mb-6 space-x-4">
-                        <div class="flex  items-center justify-center text-xl font-bold text-white rounded-full h-14 w-14">
+                        <div class="h-14 w-14">
                             <img src="{{ asset('frontend/signup.png') }}" alt="">
                         </div>
-                        <p class="text-xl font-semibold text-slate-800">Register</p>
+                        <p class="text-xl font-semibold text-[#2171a7]">Register</p>
                     </div>
                     <div class=" text-justify">
                         <span class="text-base text-slate-600">
@@ -30,11 +32,11 @@ new class extends Component {
                     </div>
                 </div>
                 <div class="flex flex-col transition-transform transform hover:scale-105">
-                    <div class="flex flex-row items-center mb-6 space-x-4">
-                        <div class="flex  items-center justify-center text-xl font-bold text-white rounded-full h-14 w-14">
+                    <div class="flex flex-row  items-center mb-6 space-x-4">
+                        <div class="h-14 w-14">
                             <img src="{{ asset('frontend/login.png') }}" alt="">
                         </div>
-                        <p class="text-xl font-semibold text-slate-800">Login</p>
+                        <p class="text-xl font-semibold text-[#2171a7]">Login</p>
                     </div>
                     <div class=" text-justify">
                         <span class="text-base text-slate-600">
@@ -44,10 +46,10 @@ new class extends Component {
                 </div>
                 <div class="flex flex-col transition-transform transform hover:scale-105">
                     <div class="flex flex-row items-center mb-6 space-x-4">
-                        <div class="flex  items-center justify-center text-xl font-bold text-white rounded-full h-14 w-14">
+                        <div class="h-14 w-14">
                             <img src="{{ asset('frontend/photo.png') }}" alt="">
                         </div>
-                        <p class="text-xl font-semibold text-slate-800">Upload Your Treasures</p>
+                        <p class="text-xl font-semibold text-[#2171a7]">Upload Your Treasures</p>
                     </div>
                     <div class=" text-justify">
                         <span class="text-base text-slate-600">
