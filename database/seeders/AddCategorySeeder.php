faad0372fa053b639df3a4502d2594394388701f
unknown <gab.quing29@gmail.com>
2024-10-15 04:08:59 +0800

    remove indention

--- database/seeders/AddCategorySeeder.php
+++ database/seeders/AddCategorySeeder.php
@@ -22,7 +22,7 @@ class AddCategorySeeder extends Seeder
                     'created_at' => Date::now(),
                 ],
                 [
-                    'name' => ' Recreational Property',
+                    'name' => 'Recreational Property',
                     'created_by' => 2,
                     'created_at' => Date::now(),
                 ],
